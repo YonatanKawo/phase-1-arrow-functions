e7dde37e733eaf2a26001a8136cfcd227d947512
Yonatan Kawo <yonas478@gmail.com>
2021-07-12 09:51:18 -0500

    commit tree 39584ce2f861403014d4a505021385bd22026310

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(){
+    return 2000/100;
+}
+const square = (parameter0) => parameter0*2;
+//square(parameter0);
+
+const add = (parameter1, parameter2) => parameter1+parameter2;
+//add(parameter1, parameter2);
+
+
+
